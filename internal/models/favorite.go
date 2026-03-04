12e789ff5a788a3744e8f3d8e49fb8f98f81b465
Claxid <hohohjiji651@gmail.com>
2026-03-04 14:32:00 +0100

    fix: add CreatedAt field to Favorite model

--- internal/models/favorite.go
+++ internal/models/favorite.go
@@ -1,9 +1,12 @@
 package models
 
+import "time"
+
 // Favorite représente un artiste favori
 type Favorite struct {
 	ID          int       `json:"id"`
 	ArtistID    int       `json:"artist_id"`
 	ArtistName  string    `json:"artist_name"`
 	ArtistImage string    `json:"artist_image"`
+	CreatedAt   time.Time `json:"created_at"`
 }
